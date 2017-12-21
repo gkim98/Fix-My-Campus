6e78fed984476258dfc0eb088525e3acc164af6f
Grant Kim <grantkim@Grants-MacBook-Pro.local>
2017-12-21 07:56:16 -0600

    table view working

---
+++ Fix My Campus/Controller/PostsViewController.swift
@@ -0,0 +1,56 @@
+//
+//  PostsViewController.swift
+//  
+//
+//  Created by Grant Kim on 12/21/17.
+//
+
+import UIKit
+
+class PostsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var postsTableView: UITableView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        postsTableView.delegate = self
+        postsTableView.dataSource = self
+        
+        postsTableView.register(UINib(nibName: "PostsCell", bundle: nil), forCellReuseIdentifier: "postsCell")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "postsCell", for: indexPath) as! PostsCell
+        
+        //test
+        let postsArray = ["One", "Two", "Three"]
+        
+        cell.messageLabel.text = postsArray[indexPath.row]
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 3
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Fix My Campus/View/Custom Cells/PostsCell.swift
@@ -0,0 +1,27 @@
+//
+//  PostsCell.swift
+//  Fix My Campus
+//
+//  Created by Grant Kim on 12/21/17.
+//  Copyright © 2017 2.5 Yellow. All rights reserved.
+//
+
+import UIKit
+
+class PostsCell: UITableViewCell {
+    
+    @IBOutlet weak var messageLabel: UILabel!
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
