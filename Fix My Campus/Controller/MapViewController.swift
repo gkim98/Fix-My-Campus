588368dc7bd7107588c38df6fe422c84772ddafe
Grant Kim <grantkim@Grants-MacBook-Pro.local>
2017-12-21 08:15:40 -0600

    add navigation controller and google maps marker

--- Fix My Campus/Controller/MapViewController.swift
+++ Fix My Campus/Controller/MapViewController.swift
@@ -16,9 +16,15 @@ class MapViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        let camera = GMSCameraPosition.camera(withLatitude: -33.6, longitude: 151.20, zoom: 6.0)
+        let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
         let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
         view = mapView
+        
+        let marker = GMSMarker()
+        marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20)
+        marker.title = "Sydney"
+        marker.snippet = "Australia"
+        marker.map = mapView
     }
 
     override func didReceiveMemoryWarning() {
