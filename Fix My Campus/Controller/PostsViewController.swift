680764dbcd6c6a6870638bacfe7c1be02dfe1a97
Grant Kim <grantkim@Grants-MacBook-Pro.local>
2017-12-25 01:45:41 -0600

    temp delete of posts view controller

---
+++ Fix My Campus/Controller/PostsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  PostsViewController.swift
+//  Fix My Campus
+//
+//  Created by Grant Kim on 12/25/17.
+//  Copyright © 2017 2.5 Yellow. All rights reserved.
+//
+
+import UIKit
+
+class PostsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
