9f13cd3f8f905e68d2cd9b585e2b7434b60a4a09
Grant Kim <grantkim@Grants-MacBook-Pro.local>
2017-12-25 01:52:27 -0600

    fixed post cells location (annoying)

--- Fix My Campus/Controller/PostsViewController.swift
+++ Fix My Campus/Controller/PostsViewController.swift
@@ -1,35 +1,56 @@
 //
 //  PostsViewController.swift
-//  Fix My Campus
 //
-//  Created by Grant Kim on 12/25/17.
-//  Copyright © 2017 2.5 Yellow. All rights reserved.
+//
+//  Created by Grant Kim on 12/21/17.
 //
 
 import UIKit
 
-class PostsViewController: UIViewController {
-
+class PostsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var postsTableView: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        postsTableView.delegate = self
+        postsTableView.dataSource = self
+        
+        postsTableView.register(UINib(nibName: "PostsCell", bundle: nil), forCellReuseIdentifier: "postsCell")
     }
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "postsCell", for: indexPath) as! PostsCell
+        
+        //test
+        let postsArray = ["One", "Two", "Three"]
+        
+        cell.messageLabel.text = postsArray[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
