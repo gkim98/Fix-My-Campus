4f57e16427856979b55c9156dda849e87e84c355
Grant Kim <grantkim@Grants-MacBook-Pro.local>
2018-01-01 08:00:33 -0600

    Add some model classes

---
+++ Fix My Campus/Model/Location.swift
@@ -0,0 +1,17 @@
+//
+//  Location.swift
+//  Fix My Campus
+//
+//  Created by Grant Kim on 1/1/18.
+//  Copyright © 2018 2.5 Yellow. All rights reserved.
+//
+
+import Foundation
+
+class Location {
+    
+    var name: String = ""
+    var posts: [Post] = [Post]()
+    
+    // add a coordinates variable
+}
---
+++ Fix My Campus/Model/Post.swift
@@ -0,0 +1,18 @@
+//
+//  Post.swift
+//  Fix My Campus
+//
+//  Created by Grant Kim on 1/1/18.
+//  Copyright © 2018 2.5 Yellow. All rights reserved.
+//
+
+import Foundation
+
+class Post {
+    
+    var sender: String = ""
+    var postTitle: String = ""
+    var postBody: String = ""
+    var isResolved: Bool = false
+    
+}
