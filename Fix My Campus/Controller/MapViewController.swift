69c880ca69a16ad91050b7b610b315300e2d5026
Grant Kim <grantkim@Grants-MacBook-Pro.local>
2018-02-05 13:07:28 -0500

    Setting up location manager

--- Fix My Campus/Controller/MapViewController.swift
+++ Fix My Campus/Controller/MapViewController.swift
@@ -8,17 +8,27 @@
 
 import UIKit
 import GoogleMaps
+import GooglePlaces
+import CoreLocation
 
-class MapViewController: UIViewController, GMSMapViewDelegate, UITableViewDelegate, UITableViewDataSource {
+class MapViewController: UIViewController, GMSMapViewDelegate, UITableViewDelegate, UITableViewDataSource, CLLocationManagerDelegate {
+    
     @IBOutlet weak var mapView: GMSMapView!
     @IBOutlet weak var mapTableView: UITableView!
+    
+    var locationManager = CLLocationManager()
+    var currentLocation: CLLocation?
+    var placesClient: GMSPlacesClient!
+    var zoomLevel: Float = 15.0
+    
+    var likelyPlaces: [GMSPlace] = []
+    var selectedPlace: GMSPlace?
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
-        mapView.camera = camera
-        mapView.delegate = self
+        setupLocationManager()
 
         let marker = GMSMarker()
         marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20)
@@ -51,6 +61,62 @@ class MapViewController: UIViewController, GMSMapViewDelegate, UITableViewDelega
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
     }
-   
+    
+    
+    //MARK: - Location Manager
+    // configuration for location manager and other setup for the map
+    func setupLocationManager() {
+        mapView.delegate = self
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.distanceFilter = 50
+        
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        placesClient = GMSPlacesClient.shared()
+    }
+    
+    // handles events for the location manager
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location: CLLocation = locations.last!
+        print("Location: \(location)")
+        
+        let camera = GMSCameraPosition.camera(withLatitude: location.coordinate.latitude, longitude: location.coordinate.longitude, zoom: zoomLevel)
+        
+        if mapView.isHidden {
+            mapView.isHidden = false
+            mapView.camera = camera
+        } else {
+            mapView.animate(to: camera)
+        }
+        
+        //listLikelyPlaces()
+    }
+    
+    // handles authorization for location manager
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        switch status {
+        case .restricted:
+            print("Location access was restricted.")
+        case .denied:
+            print("User denied access to location.")
+            // Display the map using the default location.
+            mapView.isHidden = false
+        case .notDetermined:
+            print("Location status not determined.")
+        case .authorizedAlways: fallthrough
+        case .authorizedWhenInUse:
+            print("Location status is OK.")
+        }
+    }
+    
+    // handle location manager errors
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        locationManager.stopUpdatingLocation()
+        print("Error: \(error)")
+    }
+    
+    
 }
 
